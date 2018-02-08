b62c5cfa605065034b30eda7159806638dc8a90a
dustinscarter89 <31836102+dustinscarter89@users.noreply.github.com>
2018-02-07 21:14:14 -0800

    Add files via upload

---
+++ sunglasses.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 21:03:15 2018
+
+@author: dustinscarter
+"""
+
+
+#Write a program that will inform the user to wear sunglasses if its sunny outside
+#// if it's sunny
+#    // print "Wear sunglasses!"
+#
+#// else
+#    // print "Don't wear sunglasses."
+
+outside = "cloudy"
+
+if outside == "sunny":
+    print("Wear sunglasses")
+else: 
+    print("Don't wear sunglasses")
